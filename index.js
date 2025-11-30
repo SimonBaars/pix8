c8e52efb00eecb7258f5c37527bd997d600a7078
Simon <simon.mailadres@gmail.com>
2025-11-30 13:53:53 -0600

    Add test to verify browser iframe loads example.com

--- index.js
+++ index.js
@@ -44,4 +44,12 @@ $(document).on('loaded', ev => {
       log: d
     });
   }
+  
+  // Test browser iframe functionality
+  setTimeout(() => {
+    if(typeof Pix8 !== 'undefined' && typeof Pix8.onSite === 'function') {
+      console.log('=== TESTING: Loading example.com ===');
+      Pix8.onSite('https://example.com');
+    }
+  }, 2000);
 });
